8d885a724b68f12b594aefe7cb645f9c56c410d9
tomoyukikubota <tomoyukikubota0119@gmail.com>
2022-04-01 08:17:20 +0900

    Add

--- php【基礎】/return_2.php
+++ php【基礎】/return_2.php
@@ -21,3 +21,18 @@ function num($d, $e, $f)
 
 echo num(100, 300, 500) . PHP_EOL;
 echo num(-1000, 300, 500) . PHP_EOL;
+
+//可変長引数
+
+function sum(...$numbers) {
+  
+  $total = 0;
+
+  foreach ($numbers as $number) {
+    $total += $number;
+  }
+  return $total; 
+}
+
+echo sum(1, 2, 3) . PHP_EOL;
+echo sum(10, 20, 30) . PHP_EOL;
